2d69a535e31bc3001c53efd40d8b8ddc120ec77c
RonaldFloresOrtega <ronaldpfo@byui.edu>
2020-02-26 02:13:51 -0500

    ultimate correct

--- lesson-seven/js/lazy.js
+++ lesson-seven/js/lazy.js
@@ -1,31 +1,50 @@
-const imagesToLoad = document.querySelectorAll("[data-src]");
+/*
+Lazy loading with Intersetion Observer API
+*/
 
-const imgOptions = {
-    threshold: 0.5,
-    rootMargin: "0px 0px 200px 0px"
-};
+function lazyload()
+{
+    observe();
+    
+}
+
+function observe()
+{
+    if('IntersectionObserver' in window) {
+        const imgOptions = {
+            threshold: 0.5,
+            root: null
+        };
 
 
-const loadImages = (image) => {
-	image.setAttribute("src", image.getAttribute("data-src"));
-	image.onload = () => { image.removeAttribute("data-src");
-	};
-};
-
-if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
+        let imagesToLoad = document.querySelectorAll('img[data-src]');
+        const loadImages = (image) => {
+            image.setAttribute('src', image.getAttribute('data-src'));
+            image.onload = () => {
+                image.removeAttribute('data-src');
+            };
+        };
+        
+        let callback = (items, observer) => {
+            items.forEach((item) => {
+                if(item.isIntersecting) {
+                    loadImages(item.target);
+                    observer.unobserve(item.target);
+                }
+            });
         }
-      });
-    });
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
-    });
-  } else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-    });
-  }
+
+        const observer = new IntersectionObserver(callback, imgOptions);
+
+        imagesToLoad.forEach((img) => {
+            observer.observe(img);
+        });
+    } else {
+        imagesToLoad.forEach((img) => {
+            loadImages(img);
+        });
+    }
+
+}
+
+
